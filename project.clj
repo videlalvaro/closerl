106b5873047023c3a09663be7b39c38b85cc31ae
Alvaro Videla <videlalvaro@gmail.com>
2010-08-14 03:33:54 +0800

    updates dependecies to latest Clojure release

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject closerl "1.0.0-SNAPSHOT"
   :description "Clojure Erlang Bridge using JInterface"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-                 [com.ericsson.otp/erlang "1.5.3"]])
+  :dependencies [[org.clojure/clojure "1.2.0-RC3"]
+                 [org.clojure/clojure-contrib "1.2.0-RC3"]
+                 [com.ericsson.otp/erlang "1.5.3"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
