3f8fc2a6d2ab8fb5f504d00530fa05dfa68c12e1
Alvaro Videla <videlalvaro@gmail.com>
2010-08-19 04:10:28 +0800

    adds lein run task

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0-RC3"]
                  [org.clojure/clojure-contrib "1.2.0-RC3"]
                  [com.ericsson.otp/erlang "1.5.3"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-run "1.0.0-SNAPSHOT"]])
