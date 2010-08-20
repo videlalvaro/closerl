79175e5f105d747195c62be67afe6cb8ff948c06
Alvaro Videla <videlalvaro@gmail.com>
2010-08-20 21:05:25 +0800

    updated project deps

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject closerl "1.0.0-SNAPSHOT"
   :description "Clojure Erlang Bridge using JInterface"
-  :dependencies [[org.clojure/clojure "1.2.0-RC3"]
-                 [org.clojure/clojure-contrib "1.2.0-RC3"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [com.ericsson.otp/erlang "1.5.3"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-run "1.0.0-SNAPSHOT"]])
